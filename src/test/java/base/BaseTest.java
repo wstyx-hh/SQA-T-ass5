a1807d04fef24a9da90f26e9cf6a19eef7d7167a
wstyx-hh <nora.naomivna@gmail.com>
2026-01-21 18:36:04 +0500

    changing to the edge

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -22,7 +23,7 @@ public class BaseTest {
         logger = LoggerUtil.getLogger(this.getClass());
         logger.info("===== TEST STARTED =====");
 
-        driver = new ChromeDriver();
+        driver = new EdgeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
